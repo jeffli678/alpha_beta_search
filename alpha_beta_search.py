f6622ab1fd0c7817803b61342bfaa03350b087fd
jeff <jeffli678@sina.com>
2018-02-07 19:42:06 -0500

    working alpha-beta search

--- alpha_beta_search.py
+++ alpha_beta_search.py
@@ -1,44 +1,8 @@
 """Games, or Adversarial Search (Chapter 5)"""
 
-from collections import namedtuple
-import random
-
-from utils import argmax
+import copy
 
 infinity = float('inf')
-GameState = namedtuple('GameState', 'to_move, utility, board, moves')
-
-# ______________________________________________________________________________
-# Minimax Search
-
-
-def minimax_decision(state, game):
-    """Given a state in a game, calculate the best move by searching
-    forward all the way to the terminal states. [Figure 5.3]"""
-
-    player = game.to_move(state)
-
-    def max_value(state):
-        if game.terminal_test(state):
-            return game.utility(state, player)
-        v = -infinity
-        for a in game.actions(state):
-            v = max(v, min_value(game.result(state, a)))
-        return v
-
-    def min_value(state):
-        if game.terminal_test(state):
-            return game.utility(state, player)
-        v = infinity
-        for a in game.actions(state):
-            v = min(v, max_value(game.result(state, a)))
-        return v
-
-    # Body of minimax_decision:
-    return argmax(game.actions(state),
-                  key=lambda a: min_value(game.result(state, a)))
-
-# ______________________________________________________________________________
 
 
 def alphabeta_search(state, game):
@@ -75,86 +39,14 @@ def alphabeta_search(state, game):
     beta = infinity
     best_action = None
     for a in game.actions(state):
+        print('a: ')
+        print(a)
         v = min_value(game.result(state, a), best_score, beta)
         if v > best_score:
             best_score = v
             best_action = a
+    print(v)
     return best_action
-
-
-def alphabeta_cutoff_search(state, game, d=4, cutoff_test=None, eval_fn=None):
-    """Search game to determine best action; use alpha-beta pruning.
-    This version cuts off search and uses an evaluation function."""
-
-    player = game.to_move(state)
-
-    # Functions used by alphabeta
-    def max_value(state, alpha, beta, depth):
-        if cutoff_test(state, depth):
-            return eval_fn(state)
-        v = -infinity
-        for a in game.actions(state):
-            v = max(v, min_value(game.result(state, a),
-                                 alpha, beta, depth + 1))
-            if v >= beta:
-                return v
-            alpha = max(alpha, v)
-        return v
-
-    def min_value(state, alpha, beta, depth):
-        if cutoff_test(state, depth):
-            return eval_fn(state)
-        v = infinity
-        for a in game.actions(state):
-            v = min(v, max_value(game.result(state, a),
-                                 alpha, beta, depth + 1))
-            if v <= alpha:
-                return v
-            beta = min(beta, v)
-        return v
-
-    # Body of alphabeta_cutoff_search starts here:
-    # The default test cuts off at depth d or at a terminal state
-    cutoff_test = (cutoff_test or
-                   (lambda state, depth: depth > d or
-                    game.terminal_test(state)))
-    eval_fn = eval_fn or (lambda state: game.utility(state, player))
-    best_score = -infinity
-    beta = infinity
-    best_action = None
-    for a in game.actions(state):
-        v = min_value(game.result(state, a), best_score, beta, 1)
-        if v > best_score:
-            best_score = v
-            best_action = a
-    return best_action
-
-# ______________________________________________________________________________
-# Players for Games
-
-
-def query_player(game, state):
-    """Make a move by querying standard input."""
-    print("current state:")
-    game.display(state)
-    print("available moves: {}".format(game.actions(state)))
-    print("")
-    move_string = input('Your move? ')
-    try:
-        move = eval(move_string)
-    except NameError:
-        move = move_string
-    return move
-
-
-def random_player(game, state):
-    """A player that chooses a legal move at random."""
-    return random.choice(game.actions(state))
-
-
-def alphabeta_player(game, state):
-    return alphabeta_search(state, game)
-
 
 # ______________________________________________________________________________
 # Some Sample Games
@@ -208,6 +100,111 @@ class Game:
                     return self.utility(state, self.to_move(self.initial))
 
 
+class Poker(Game):
+
+    def actions(self, state):
+        """Return a list of the allowable moves at this point."""
+
+        # state is organized as
+        # {'card_sets' : [card_set_0, card_set_1], "moving_side" : 0, "last_ply" : {'card': 7 , 'num' : 2}}
+
+        card_sets = state['card_sets']
+        moving_side = state['moving_side']
+        last_ply = state['last_ply']
+
+        candidate_set = card_sets[moving_side]
+        last_card = last_ply['card']
+        last_num = last_ply['num']
+
+        possible_move = []
+
+        if last_card:
+            # the last ply is real card
+            for i in range(last_card + 1, len(candidate_set) ):
+                diff_num = candidate_set[i] - last_num
+                if diff_num >= 0:
+                    possible_move.append({'card' : i, 'num' : last_num})
+
+            # pass if we do not have anythin bigger than that
+            # note that sometimes we want to pass even if we can beat it
+            # later refine this
+            if len(possible_move) == 0:
+                possible_move.append({'card' : None, 'num' : 0})
+
+        else:
+            # the opponent past the last round, and we play whatever we want
+            for i in range(len(candidate_set)):
+                if candidate_set[i] > 0:
+                    for j in range(1, candidate_set[i] + 1):
+                        possible_move.append({'card' : i, 'num' : j})
+
+        # print('state: ')
+        # print(state)
+        # print(possible_move)
+        # print('\n')
+        return possible_move
+
+
+    def result(self, state, move):
+        """Return the state that results from making a move from a state."""
+
+        new_state = copy.deepcopy(state)
+        card_sets = new_state['card_sets']
+        moving_side = new_state['moving_side']
+        last_ply = new_state['last_ply']
+
+        # make a copy; otherwise it is still linked
+        changed_set = card_sets[moving_side]
+        changed_card = move['card']
+        changed_num = move['num']
+
+        # reduce the number of cards
+        if changed_card != None:
+            changed_set[changed_card] -= changed_num
+
+        # change the moving side
+        if moving_side == 1:
+            moving_side = 0
+        else:
+            moving_side = 1
+
+        # mark the new last_ply
+        last_ply = move
+
+        new_state = {'card_sets' : card_sets, 'moving_side' : moving_side, 'last_ply' : last_ply}
+        # print('new_state: ')
+        # print(new_state)
+        return new_state
+
+    def utility(self, state, player):
+        """Return the value of this final state to player."""
+        # note this is related to how to find the best moves of the loser
+        # moving_side is the side to move
+        # if it is already a terminal state when you are moving_side
+        # if meanns the other people wins
+        # a little bit tricky and intuitive here; be CRREFUL
+        if player != state['moving_side']:
+            return 1
+        else:
+            return -1
+
+    def terminal_test(self, state):
+        """Return True if this is a final state for the game."""
+        # return not self.actions(state)
+        card_sets = state['card_sets']
+        for card_set in card_sets:
+            if sum(card_set) == 0:
+                # print(state)
+                # print('terminal_test')
+                return True
+
+
+        return False
+
+    def to_move(self, state):
+        return state['moving_side']
+
+
 class Fig52Game(Game):
     """The game represented in [Figure 5.2]. Serves as a simple test case."""
 
@@ -215,7 +212,7 @@ class Fig52Game(Game):
                  B=dict(b1='B1', b2='B2', b3='B3'),
                  C=dict(c1='C1', c2='C2', c3='C3'),
                  D=dict(d1='D1', d2='D2', d3='D3'))
-    utils = dict(B1=3, B2=12, B3=8, C1=2, C2=4, C3=6, D1=14, D2=5, D3=2)
+    utils = dict(B1=2, B2=12, B3=8, C1=3, C2=4, C3=6, D1=14, D2=5, D3=2)
     initial = 'A'
 
     def actions(self, state):
@@ -236,109 +233,17 @@ class Fig52Game(Game):
     def to_move(self, state):
         return 'MIN' if state in 'BCD' else 'MAX'
 
-
-class Fig52Extended(Game):
-    """Similar to Fig52Game but bigger. Useful for visualisation"""
-
-    succs = {i:dict(l=i*3+1, m=i*3+2, r=i*3+3) for i in range(13)}
-    utils = dict()
-
-    def actions(self, state):
-        return sorted(list(self.succs.get(state, {}).keys()))
-
-    def result(self, state, move):
-        return self.succs[state][move]
-
-    def utility(self, state, player):
-        if player == 'MAX':
-            return self.utils[state]
-        else:
-            return -self.utils[state]
-
-    def terminal_test(self, state):
-        return state not in range(13)
-
-    def to_move(self, state):
-        return 'MIN' if state in {1, 2, 3} else 'MAX'
-
-class TicTacToe(Game):
-    """Play TicTacToe on an h x v board, with Max (first player) playing 'X'.
-    A state has the player to move, a cached utility, a list of moves in
-    the form of a list of (x, y) positions, and a board, in the form of
-    a dict of {(x, y): Player} entries, where Player is 'X' or 'O'."""
-
-    def __init__(self, h=3, v=3, k=3):
-        self.h = h
-        self.v = v
-        self.k = k
-        moves = [(x, y) for x in range(1, h + 1)
-                 for y in range(1, v + 1)]
-        self.initial = GameState(to_move='X', utility=0, board={}, moves=moves)
-
-    def actions(self, state):
-        """Legal moves are any square not yet taken."""
-        return state.moves
-
-    def result(self, state, move):
-        if move not in state.moves:
-            return state  # Illegal move has no effect
-        board = state.board.copy()
-        board[move] = state.to_move
-        moves = list(state.moves)
-        moves.remove(move)
-        return GameState(to_move=('O' if state.to_move == 'X' else 'X'),
-                         utility=self.compute_utility(board, move, state.to_move),
-                         board=board, moves=moves)
-
-    def utility(self, state, player):
-        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        return state.utility if player == 'X' else -state.utility
-
-    def terminal_test(self, state):
-        """A state is terminal if it is won or there are no empty squares."""
-        return state.utility != 0 or len(state.moves) == 0
-
-    def display(self, state):
-        board = state.board
-        for x in range(1, self.h + 1):
-            for y in range(1, self.v + 1):
-                print(board.get((x, y), '.'), end=' ')
-            print()
-
-    def compute_utility(self, board, move, player):
-        """If 'X' wins with this move, return 1; if 'O' wins return -1; else return 0."""
-        if (self.k_in_row(board, move, player, (0, 1)) or
-                self.k_in_row(board, move, player, (1, 0)) or
-                self.k_in_row(board, move, player, (1, -1)) or
-                self.k_in_row(board, move, player, (1, 1))):
-            return +1 if player == 'X' else -1
-        else:
-            return 0
-
-    def k_in_row(self, board, move, player, delta_x_y):
-        """Return true if there is a line through move on board for player."""
-        (delta_x, delta_y) = delta_x_y
-        x, y = move
-        n = 0  # n is number of moves in row
-        while board.get((x, y)) == player:
-            n += 1
-            x, y = x + delta_x, y + delta_y
-        x, y = move
-        while board.get((x, y)) == player:
-            n += 1
-            x, y = x - delta_x, y - delta_y
-        n -= 1  # Because we counted move itself twice
-        return n >= self.k
+    # def display(self, state):
+        # print(state)
 
 
-class ConnectFour(TicTacToe):
-    """A TicTacToe-like game in which you can only make a move on the bottom
-    row, or in a square directly above an occupied square.  Traditionally
-    played on a 7x6 board and requiring 4 in a row."""
+# game = Fig52Game()
+# best_action = alphabeta_search('A', game)
+# print(best_action)
 
-    def __init__(self, h=7, v=6, k=4):
-        TicTacToe.__init__(self, h, v, k)
-
-    def actions(self, state):
-        return [(x, y) for (x, y) in state.moves
-                if y == 1 or (x, y - 1) in state.board]
+game = Poker()
+# init_state = {'card_sets' : [[1, 0, 1], [0, 1, 1]], "moving_side" : 0, "last_ply" : {'card': None , 'num' : 0}}
+card_sets = [[2, 2, 2, 0, 3, 3, 0, 1], [0, 0, 0, 2, 0, 0, 2, 0]]
+init_state = {'card_sets' : card_sets, "moving_side" : 0, "last_ply" : {'card': None , 'num' : 0}}
+best_action = alphabeta_search(init_state, game)
+print(best_action)
